213bec4c3983108929d7f1518b429214e5fbe2e2
nuri <nuriddin@utec.kr>
2023-04-17 15:19:02 +0900

    methodChannel working on android side.

--- android/app/src/main/kotlin/com/example/method_channel_test_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/method_channel_test_app/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.example.method_channel_test_app
 
+import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private val channelName = "nurichanneltoast";
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        var channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName);
+
+        channel.setMethodCallHandler { call, result ->
+            if (call.method == "showToast"){
+                Toast.makeText(this, "my channel toast", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
 }
